b2e2058a89bc7b14fd90dda036f24b2ba39c497f
지현 <cwlgusc@gmail.com>
2023-09-13 14:46:23 +0900

    feat: problem 4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,28 @@
 package onboarding;
 
 public class Problem4 {
+    public static void main(String[] args) {
+        String a = "AAaae";
+        String solution = solution(a);
+        System.out.println("solution = " + solution);
+    }
+
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+            char currentChar = word.charAt(i);
+            if (Character.isLetter(currentChar)) {
+                if (Character.isUpperCase(currentChar)) {
+                    int i1 = 'Z' - currentChar + 'A';
+                    sb.append((char) i1);
+                }else if (Character.isLowerCase(currentChar)) { // 소문자인 경우
+                    int shiftedValue = 'Z' - currentChar + 'a';
+                    sb.append((char) shiftedValue);
+                }
+            } else {
+                sb.append(currentChar);
+            }
+        }
+        return sb.toString();
     }
 }
