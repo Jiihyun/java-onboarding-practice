8b85044353083a7c766fe034d792f4db3c3ea000
지현 <cwlgusc@gmail.com>
2023-09-20 23:41:23 +0900

    feat: count visitors

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,10 +1,22 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        Map<String, Integer> hashMap = new HashMap<>();
+
+        int count = 1;
+        for (String visitor : visitors) {
+            if (hashMap.containsKey(visitor)) {
+                count++;
+            }
+            hashMap.put(visitor, count);
+        }
+
         List<String> answer = Collections.emptyList();
         return answer;
     }
